7686880a6e1cfe3797788e3c318dab81fc50d2c6
alla <varlamova.alla.va@gmail.com>
2019-05-18 14:46:08 +0300

    Add JS#1

---
+++ resources/js/js1.js
@@ -0,0 +1,38 @@
+console.log('Hello 3');
+
+var $age=14;
+alert($age);
+console.log($age);
+
+var firstName = 'Vasia';
+alert(firstName);
+console.log(firstName);
+
+// var bool = true; /* false */
+// alert(true);
+// console.log(true);
+
+// var COLOR = 'red';
+
+// var n1 = 10;
+// var n2 = 20;
+// if(bool) {
+//     alert('It is TRue: n1 + n2 = ' + (n1 + n2) );
+// }else{
+//     alert('It is False: n1 - n2 = '  + (n1 - n2) );
+// }
+// var searchTxt;
+// document.getElementById("searchTxt").value;
+
+
+// document.querySelector('p').innerText='Hello new'
+
+
+var a  = +prompt('Enter A');
+var b  = +prompt('Enter B');
+console.log(typeof a);
+var c  = +prompt('Enter C');
+var text  = prompt('Enter Text');
+console.log(typeof text);
+var d = b*b - 4*a*c;
+alert('D = b*b - 4*a*c = ' + b +'*'+b+'+4*'+a+'*'+c+ ' = '+ d);
