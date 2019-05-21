cace96b6f966404f362ceef4012ad815ef85ea7a
Dmitry Prokopenko <6558822@gmail.com>
2019-05-21 23:06:51 +0300

    Add EliNext 2

---
+++ elinext/elinext_js.js
@@ -0,0 +1,9 @@
+alert('Hello!');
+
+var yes_no  =  prompt('are you realy form EliNext ?? (yes/no)');
+
+ if(yes_no == 'yes'){
+    alert('Good very Good! ha ha ha!');
+}else{
+     alert('Not Good... You should work on Elinext');
+}
