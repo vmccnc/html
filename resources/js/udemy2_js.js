9234d4033aac87e081356bfb64250d5eef0be9a5
Dmitry Prokopenko <6558822@gmail.com>
2019-05-22 14:02:02 +0300

    EliNext . Add Navigation

---
+++ resources/js/udemy2_js.js
@@ -0,0 +1,22 @@
+console.log('Hello 3');
+
+var a  = prompt('Enter any word: ');
+ 
+alert(a.length);
+console.log(a.length);
+
+//var firstName = 'Vasia';
+//alert(firstName);
+//console.log(firstName);
+//
+// 
+//
+//
+//var a  = +prompt('Enter A');
+//var b  = +prompt('Enter B');
+//console.log(typeof a);
+//var c  = +prompt('Enter C');
+//var text  = prompt('Enter Text');
+//console.log(typeof text);
+//var d = b*b - 4*a*c;
+//alert('D = b*b - 4*a*c = ' + b +'*'+b+'+4*'+a+'*'+c+ ' = '+ d);
