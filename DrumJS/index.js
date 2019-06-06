fc10469d91a4f930c37d7647095dc375d873889e
Dmitry Prokopenko <6558822@gmail.com>
2019-06-06 22:15:53 +0300

    Add Udemy2 JS sect 20

--- DrumJS/index.js
+++ DrumJS/index.js
@@ -1,9 +1,27 @@
 
 
 
+
 for(var i =0; i<document.querySelectorAll('button').length; i++){
    document.querySelectorAll('button')[i].addEventListener('click', function() {
-         alert('Button is clicked');   
+         console.log('Button is clicked ' + this.innerHTML );
+       
+       
+       
+       if(this.innerHTML=='l'){
+            var audio = new Audio('sounds/snare.mp3');
+            audio.play();
+       }else if(this.innerHTML=='k'){
+            var audio = new Audio('sounds/kick-bass.mp3');
+            audio.play();
+       }else if(this.innerHTML=='j'){
+            var audio = new Audio('sounds/crash.mp3');
+            audio.play();
+       }else {
+           var audio = new Audio('sounds/tom-2.mp3');
+            audio.play();
+       }
+       
  } ); 
 }
     
@@ -14,6 +32,36 @@ for(var i =0; i<document.querySelectorAll('button').length; i++){
 //    alert('I got clicked');
 //}
 
-document.querySelectorAll('.tt').onclick = function(){
-    alert('tt is clicked!')
+//document.querySelectorAll('.tt').onclick = function(){
+//    alert('tt is clicked!')
+//}
+
+
+
+
+
+
+
+
+function add(a, b){
+    return a+b;
 }
+
+function mult(a, b){
+    return a*b;
+}
+
+function substruct(a, b){
+    return a-b;
+}
+function dev(a, b){
+    return a/b;
+}
+
+function calculator(a, b, operator){
+    return operator(a, b);
+}
+
+
+//debugger;
+//calculator(5, 2, mult);
