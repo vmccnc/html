0c1c7b646b12e38c4f5a35da9b9cac11da434978
Dmitry Prokopenko <6558822@gmail.com>
2019-06-12 22:53:12 +0300

    Add udemy2. JQuery next

--- jquery.js
+++ jquery.js
@@ -7,3 +7,26 @@ $('h1').css('color', 'red');
 // document.querySelectorAll('button')
 
 $('button').css('border', '1px solid red');
+$('img').attr('src');
+//$('img').attr('href', 'resources/img/diplomaNadia/mobileWithNadia1.png');
+//$('a').attr('href', 'http://dmitrypro.beget.tech/homework3.html');
+
+$('h1').click(function(){
+    $('h1').css('color', 'green');
+});
+2
+$('h2').click(function(){
+    $('h2').css('color', 'yellow');
+});
+
+$('button').click(function(){
+    $('h2').css('color', 'purple');
+});
+
+$('input').keypress(function(){
+    console.log(event.key);
+});
+
+$(document).keypress(function(){
+    $('h1').text(event.key);
+});
